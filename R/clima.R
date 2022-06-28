35764621e175f41e8e7dcbb1d716cc6d819f7aaf
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2022-06-28 14:00:46 -0500

    update example

--- R/clima.R
+++ R/clima.R
@@ -140,9 +140,9 @@ clima <- function(bios, tmin = NULL, tmax = NULL, tavg = NULL, prcp = NULL,
     chituc <- ventana(tavg, period, circular) / period
   }
   # Window message
-  if (any(c(8:11, 16:19, 24:27) %in% bios)) {
+  if (any(c(8:11, 16:19) %in% bios)) {
     message(
-      paste0(paste0("Bio", sprintf("%02d", c(8:11, 16:19, 24:27)[c(8:11, 16:19, 24:27) %in% bios]),
+      }paste0(paste0("Bio", sprintf("%02d", c(8:11, 16:19, 24:27)[c(8:11, 16:19) %in% bios]),
                     collapse = ", "),
              " was(were) built with a period of ", period,
              " units with", if(circular == FALSE) "out", " circularity.")
